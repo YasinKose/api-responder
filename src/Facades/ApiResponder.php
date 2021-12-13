bc9214cc9ce677ef06ca9ffbf8ada34a7f78f7f4
Yasin Köse <yasin.kose.123@gmail.com>
2021-12-13 14:09:10 +0300

    Create ApiResponder Facade

---
+++ src/Facades/ApiResponder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace YasinKose\ApiResponder\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static notFound(string $message, $errors = [])
+ * @method static ok(string $message = "OK", $attr = [])
+ * @method static unAuthenticated(string $message = "Unauthorized", $errors = [])
+ * @method static forbidden(string $message = "Forbidden", $errors = [])
+ * @method static error(string $message = null, $errors = [])
+ * @method static created($attr = null)
+ * @method static failedValidation(string $message = "Unprocessable Entity", $errors = [])
+ * @method static noContent(string $message = "No Content", $errors = [])
+ */
+class ApiResponder extends Facade
+{
+    /**
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'api-responder';
+    }
+}
